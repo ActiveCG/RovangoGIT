c1caa027fbb498835081ed8e0c9dbfc73dd1c264
activeaudio <sivers12@student.aau.dk>
2016-09-17 14:20:29 +0200

    *added jenkins script

---
+++ Dadiu Game MP1/Assets/Editor/ProjectBuilder.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using System;
+
+public class ProjectBuilder {
+
+	static string[] SCENES = FindEnabledEditorScenes();
+	//static string path = "C:/Builds";
+
+	static string APP_NAME = "PiggyGame";
+	static string TARGET_DIR = "C:/Users/SULEZ/Documents/DevelopmentJenkins/Unitybuilds";
+
+
+	public static void BuildProject(){
+		performAndroidBuild ();
+		//string[] scenes = FindEnabledEditorScenes(); //["Scenes/Lake Scene.unity", "Scenes/game over.unity", "Scenes/you win.unity"];
+		//Directory.CreateDirectory(path);
+		//string BuildFolder = System.Diagnostics.Process.Start (Directory.GetCurrentDirectory() + "/" + BuildFolder + "/");
+	}
+
+	static void performAndroidBuild(){
+		//Set the path to the Android SDK on the machine, since Unity cannot retain the state properly
+		//AndroidSDKFolder.Path = "${ANDROID_HOME}";
+		string target_dir = APP_NAME + ".apk";
+		GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTarget.Android,BuildOptions.None);
+	}
+
+
+	private static string[] FindEnabledEditorScenes() {
+		List<string> EditorScenes = new List<string>();
+		foreach(EditorBuildSettingsScene scene in EditorBuildSettings.scenes) {
+			if (!scene.enabled) continue;
+			EditorScenes.Add(scene.path);
+		}
+		return EditorScenes.ToArray();
+	}
+
+	static void GenericBuild(string[] scenes, string target_dir, BuildTarget build_target, BuildOptions build_options)
+	{
+		EditorUserBuildSettings.SwitchActiveBuildTarget(build_target);
+		string res = BuildPipeline.BuildPlayer(scenes,target_dir,build_target,build_options);
+		if (res.Length > 0) {
+			throw new Exception("BuildPlayer failure: " + res);
+		}
+	}
+
+}
